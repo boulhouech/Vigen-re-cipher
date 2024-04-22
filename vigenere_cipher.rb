f7b42edccacf2507bb91c2d25d706b6c6923c4b8
boulhouech <slim.boulahouech@gmail.com>
2024-04-22 15:00:01 +0100

    Determine the shift values for the keyword

--- vigenere_cipher.rb
+++ vigenere_cipher.rb
@@ -1,6 +1,14 @@
 def vigenere_cipher(plaintext, keyword)
   repeated_keyword= ""
+  keyword_length = keyword.length
+  encrypted_text = ""
+
   plaintext.each_with_index do |char, index|
     repeated_keyword =+ keyword[index % plaintext.lenth]
   end
+
+  shifts = repeated_keyword.upcase.chars.map do |char|
+    char.ord - 'A'.ord
+  end
+
 end
